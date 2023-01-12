d36c12569695a9f37f8f67c20faa1fe476aed299
Wallace Leonel <wallace.leonell@outlook.com>
2023-01-12 14:31:21 -0300

    feat: manipulando array, classes e listas.

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,73 @@
-﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.byteBank.Util;
 
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+//TestaArrayInt();
+
+void TestaArrayInt()    
+{
+}
+
+//TestaBuscarPalavra();
+
+void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i< arrayDePalavras.Length; i ++)
+    {
+        Console.Write($"Digite {i + 1} palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+    Console.Write("Digite palavra a ser encontrada");
+    var busca = Console.ReadLine();
+
+    foreach(var palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"palavra encontada = {busca}.");
+            break;
+        }
+    }
+}
+
+//Array amostra = new double[5];
+
+//amostra.SetValue(5.9, 0);
+//amostra.SetValue(1.8, 1);
+//amostra.SetValue(7.1, 2);
+//amostra.SetValue(10.9, 3);
+//amostra.SetValue(6.8, 4);
+//amostra.SetValue(7.9, 5);
+
+//TestaMediana(amostra);
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))         
+    {
+        Console.WriteLine(" Array esta vazio ou nulo");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :( numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+    Console.WriteLine($"Com banse na amostra a mediana é {mediana}");
+
+}
+
+void TestaArrayContaCorrentes()
+{
+    ListaContasCorrentes listaContas = new ();
+    listaContas.Adicionar(new ContaCorrente(874));
+    listaContas.Adicionar(new ContaCorrente(875));
+    listaContas.Adicionar(new ContaCorrente(876));
+
+}
+
+TestaArrayContaCorrentes();
---
+++ bytebank_ATENDIMENTO/byteBank.Util/ListaContasCorrentes.cs
@@ -0,0 +1,44 @@
+﻿using bytebank.Modelos.Conta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ATENDIMENTO.byteBank.Util
+{
+    public class ListaContasCorrentes
+    {
+        private ContaCorrente[] _itens = null;
+        private int  _proximaPosicao = 0;
+
+        public ListaContasCorrentes(int tamanhoInicial = 5)
+        {
+            _itens = new ContaCorrente[tamanhoInicial];
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            Console.WriteLine($" Indice  - Conta atual {_proximaPosicao}");
+            VerrificaCapacidade(_proximaPosicao);
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+        private void VerrificaCapacidade(int tamanoPermitido)
+        {
+            if(_itens.Length >= tamanoPermitido)
+            {
+                return;
+            }
+            Console.WriteLine("Aumentando a capaciadade da lista");
+            ContaCorrente[] novoArray = new ContaCorrente[tamanoPermitido];
+
+            for ( int i = 0; i < _itens.Length; i++ )
+            {
+                novoArray[i] = _itens[i];
+            }
+            _itens= novoArray;
+        }
+    }
+}
