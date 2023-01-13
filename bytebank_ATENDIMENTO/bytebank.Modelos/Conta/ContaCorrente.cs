96e2e1349b5e367ff18cfe08fc2a8fc6681dee95
Wallace Leonel <wallace.leonell@outlook.com>
2023-01-13 02:51:10 -0300

    refactor: aplica clean code no projeto.

--- bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
+++ bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
@@ -104,14 +104,23 @@
 			TotalDeContasCriadas++;
 		}
 
-		public override string ToString()
+        public ContaCorrente(int numero_agencia)
+        {
+            Numero_agencia = numero_agencia;
+            Conta = Guid.NewGuid().ToString().Substring(0, 8);
+            Titular = new Cliente();
+            TotalDeContasCriadas += 1;
+        }
+
+        public override string ToString()
 		{
 
 			return $" === DADOS DA CONTA === \n" +
 				   $"Número da Conta : {this.Conta} \n" +
-				   $"Titular da Conta: {this.Titular.Nome} \n" +
+                   $"Número da Agência : {this.Nome_Agencia} \n" +
+                   $"Titular da Conta: {this.Titular.Nome} \n" +
 				   $"CPF do Titular  : {this.Titular.Cpf} \n" +
-				   $"Profissão do Titular: { this.Titular.Profissao}";
+				   $"Profissão do Titular: { this.Titular.Profissao}\n\n";
 		}
 
         public int CompareTo(ContaCorrente? other)
