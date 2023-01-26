be7a6a6567a9618bbeb88e0732b0e3e77d4b6f48
Wallace Leonel <wallace.leonel@playmove.com.br>
2023-01-26 16:28:18 -0300

    feat: cria dbContext

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,6 +4,8 @@ namespace FilmesApi.Models
 {
     public class Filme
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "titulo do fime e obrigatorio")]
