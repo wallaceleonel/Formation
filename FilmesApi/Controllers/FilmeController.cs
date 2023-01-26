0f51fb493a92dfebac69dce3d8e1071211affeb4
Wallace Leonel <wallace.leonell@outlook.com>
2023-01-26 12:06:03 -0300

    feat: cria metodos aplicando padrão rest.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,39 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(BuscarFilmeId),
+               new { id = filme.Id },
+               filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> BuscarFilmes([FromQuery] int skip = 0,
+            [FromQuery] int take = 50)
+        {
+            return filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarFilmeId(int id)
+        {
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            return Ok(filme);
+        }
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "titulo do fime e obrigatorio")]
+        [MaxLength(500, ErrorMessage = "O titulo não pode ter mais do que 500 caracteres")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage ="O gênero do filme é obrigatorio")]
+        [MaxLength(50, ErrorMessage ="O gênero não pode ter mais do que 50 caracteres")]
+        public string Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "duração deve ter entre 70 e 600 minutos")]
+        public int Duracao { get; set; }    
+    }
+}
