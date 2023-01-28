8dc89cb034f4efd30a2f994b33ea784500a10296
wallaceleonel <wallace.leonell@outlook.com>
2023-01-28 20:10:27 -0200

    feat: cria buscas a novos verbos seguindo o padrão restfull, documenta swagger de forma parcial.

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,6 +1,10 @@
-﻿using FilmesApi.Data;
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
 using FilmesApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace FilmesApi.Controllers
 {
@@ -11,15 +15,25 @@ namespace FilmesApi.Controllers
         private static readonly List<Filme> filmes = new();
         
         private FilmeContext _context;
+        private IMapper _mapper;
 
-        public  FilmeController(FilmeContext context)
+        public  FilmeController(FilmeContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpPost]
-        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
         {
+            Filme filme = _mapper.Map<Filme>(filmeDto);
             _context.Filmes.Add(filme);
             _context.SaveChanges();
             return CreatedAtAction(nameof(BuscarFilmeId),
@@ -27,13 +41,26 @@ namespace FilmesApi.Controllers
                filme);
         }
 
+        /// <summary>
+        /// Retorna lista de filmes do banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso inserção seja feita com sucesso</response>
         [HttpGet]
-        public IEnumerable<Filme> BuscarFilmes([FromQuery] int skip = 0,
+        public IEnumerable<ReadFilmeDto> BuscarFilmes([FromQuery] int skip = 0,
             [FromQuery] int take = 50)
         {
-            return _context.Filmes.Skip(skip).Take(take);
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip
+                (skip).Take(take));
         }
 
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpGet("{id}")]
         public IActionResult BuscarFilmeId(int id)
         {
@@ -42,7 +69,76 @@ namespace FilmesApi.Controllers
             {
                 return NotFound();
             }
-            return Ok(filme);
+            var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+            return Ok(filmeDto);
         }
+
+        /// <summary>
+        /// Atualiza dados de um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso inserção seja feita com sucesso</response>
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody]
+            UpdateFilmeDto filmeDto)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+                filme => filme.Id == id);
+            if (filme == null) return NotFound();
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Atualiza dados de um  filme de forma parcial no banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso inserção seja feita com sucesso</response>
+        [HttpPatch("{id}")]
+        public IActionResult AtualizaFilmeParcial(int id,
+            JsonPatchDocument<UpdateFilmeDto> patch)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+                filme => filme.Id == id);
+            if (filme == null) return NotFound();
+
+            var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+            patch.ApplyTo(filmeParaAtualizar, ModelState);
+
+            if(!TryValidateModel(filmeParaAtualizar))
+            {
+                return ValidationProblem(ModelState);
+
+            }
+
+            _mapper.Map(filmeParaAtualizar, filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Remove um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso a remoção seja feita com sucesso</response>
+        [HttpDelete]
+        public IActionResult DeletaFilme(int id)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+                filme=> filme.Id == id);
+            if (filme == null) return NotFound();
+            _context.Filmes.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+
+        }
+
     }
 }
---
+++ FilmesApi/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace FilmesApi.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+        public string Titulo { get; set; }
+
+        public string Genero { get; set; }
+
+        public string Duracao { get; set; }
+
+        public DateTime HoeraDaConsulta { get; set; } = DateTime.Now;
+    }
+}
