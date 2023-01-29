5ad5e4f0ce7090fc2d9a72e144e41054da0e0e08
wallaceleonel <wallace.leonell@outlook.com>
2023-01-28 23:02:28 -0200

    refactor: ajusta testes.

--- estacionamento/Estacionamento.Test/PatioTest.cs
+++ estacionamento/Estacionamento.Test/PatioTest.cs
@@ -1,17 +1,13 @@
 ﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Estacionamento.Test
 {
     public class PatioTest
     {
-        [Fact]
+        [Fact(DisplayName ="teste n 1")]
+        [Trait("Funcionalidade", "Faturamento")]
         public void ValidaFaturamento()
         {
             //Arrange
@@ -32,6 +28,34 @@ namespace Estacionamento.Test
             //Assert
             Assert.Equal(2, Faturamento);
         }
-    }
 
+
+        [Theory]
+        [InlineData("Andre", "ASD-1234", "preto", "Gol")]
+        [InlineData("Julia", "WAL-2609", "azul", "Fox")]
+        [InlineData("Nicolas", "GTA-1157", "preto", "Fusca")]
+        public void ValidarFaturamentoDeVariosVeiculos(string proprietario,
+                                                       string placa,
+                                                       string cor,
+                                                       string modelo)
+        {
+
+            //Arrange
+            Patio estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = proprietario;
+            veiculo.Placa = placa; 
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturento = estacionamento.TotalFaturado();
+
+            //Assert
+
+            Assert.Equal(2, faturento);
+        }
+    }
 }
