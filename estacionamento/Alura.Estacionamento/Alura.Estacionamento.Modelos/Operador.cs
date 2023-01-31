f7f156e1e12d94cf40d4f7eab35be7bba2f0ded0
wallaceleonel <wallace.leonell@outlook.com>
2023-01-31 16:02:13 -0300

    refactor: efetua correções e cria mais testes

---
+++ estacionamento/Alura.Estacionamento/Alura.Estacionamento.Modelos/Operador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Estacionamento.Alura.Estacionamento.Modelos
+{
+    public class Operador
+    {
+        private string _nome;
+        private string _matricula;
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Matricula { get => _matricula; set => _matricula = value; }
+
+        public Operador()
+        {
+            this.Matricula = new Guid().ToString().Substring(0, 8);
+        }
+
+        public override string ToString()
+        {
+            return $"Operador: {this.Nome} \n" +
+                   $"Matricula: {this.Matricula}";
+        }
+    }
+}
